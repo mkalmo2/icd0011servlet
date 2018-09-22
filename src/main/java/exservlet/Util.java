876af3bd5a4e4849517340f065a3bc3e3aa88823
Märt Kalmo <mart.kalmo@gmail.com>
2018-09-22 10:25:50 +0300

    updated Util class

--- src/main/java/exservlet/Util.java
+++ src/main/java/exservlet/Util.java
@@ -1,32 +1,16 @@
 package exservlet;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 public class Util {
 
-    public static String readFileFromClasspath(String pathOnClasspath) {
-        try (InputStream is = Util.class.getClassLoader().getResourceAsStream(pathOnClasspath)) {
-            if (is == null) {
-                throw new IllegalStateException("can't load file: " + pathOnClasspath);
-            }
+    public static String readStream(InputStream is) {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
 
-            BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
-
-            return buffer.lines().collect(Collectors.joining("\n"));
-
-        } catch (IOException e) {
-            throw new RuntimeException();
-        }
-    }
-
-    public static String asString(InputStream is) throws IOException {
-        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(is))) {
-            return buffer.lines().collect(Collectors.joining("\n"));
-        }
+        return buffer.lines().collect(Collectors.joining("\n"));
     }
 
 }
