a41d10e8d07a0f0c7ca87ed1694e552427c11dd0
Märt Kalmo <mart.kalmo@gmail.com>
2020-10-20 10:12:57 +0300

    Util.readStream fixed

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 public class Util {
 
     public static String readStream(InputStream is) {
-        try (Scanner scanner = new Scanner(is, StandardCharsets.UTF_8.name())) {
-            return scanner.useDelimiter("\\A").next();
+        try (Scanner scanner = new Scanner(is,
+                StandardCharsets.UTF_8.name()).useDelimiter("\\A")) {
+
+            return scanner.hasNext() ? scanner.next() : "";
         }
     }
 
