a13bacd961c90028b1419cc27cb21c21a3bf6a16
Märt Kalmo <mart.kalmo@gmail.com>
2023-09-20 09:22:37 +0300

    added ObjectMapper list ex

---
+++ src/main/java/json/JsonConverterList.java
@@ -0,0 +1,14 @@
+package json;
+
+public class JsonConverterList {
+
+    public static void main(String[] args) {
+
+        String json = "[{\"id\": 1}, {\"id\": 2}]";
+
+        // List<Post> posts = ...
+
+    }
+
+
+}
