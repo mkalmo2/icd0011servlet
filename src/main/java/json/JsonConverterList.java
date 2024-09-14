c70b67a0ab2ee03d81d16c05ba9507b2cb3e68bc
Märt Kalmo <mart.kalmo@gmail.com>
2024-09-14 10:23:57 +0300

    Updated JsonConverterList ex

--- src/main/java/json/JsonConverterList.java
+++ src/main/java/json/JsonConverterList.java
@@ -4,7 +4,7 @@ public class JsonConverterList {
 
     public static void main(String[] args) {
 
-        String json = "[{\"id\": 1}, {\"id\": 2}]";
+        String json = "[{\"id\": 1}, {\"title\": \"hello\"}]";
 
         // List<Post> posts = ...
 
